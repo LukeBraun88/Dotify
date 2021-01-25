673cf0a023d6f1b5968d292ab7474b04dfc35454
luke <lukebraun88@att.net>
2021-01-25 12:09:34 -0600

    SSL fix

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,5 +1,4 @@
 const config = require('./index');
-
 const db = config.db;
 const username = db.username;
 const password = db.password;
@@ -13,11 +12,17 @@ module.exports = {
     database,
     host,
     dialect: 'postgres',
-    seederStorage: "sequelize",
+    seederStorage: 'sequelize',
   },
   production: {
     use_env_variable: 'DATABASE_URL',
     dialect: 'postgres',
     seederStorage: 'sequelize',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
   },
 };
