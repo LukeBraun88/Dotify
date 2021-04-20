dde04eed4175762847c40e09abc828def5e62072
luke <lukebraun88@att.net>
2021-04-20 11:00:38 -0500

    added audio wrapper and fixed scroll

--- frontend/src/components/MusicBar/index.js
+++ frontend/src/components/MusicBar/index.js
@@ -1,15 +1,28 @@
 import "./MusicBar.css"
+import AudioPlayer from 'react-h5-audio-player';
+import 'react-h5-audio-player/lib/styles.css';
+
+const Player = () => (
+    <AudioPlayer
+        autoPlay
+        src="http://example.com/audio.mp3"
+        onPlay={e => console.log("onPlay")}
+    // other props here
+    />
+);
 
 
 export default function MusicBar() {
     return (
         <div className="music-bar">
-            <div className="song-info">
+            {/* <div className="song-info">
                 <div className="album-image"></div>
-                <div className="song-artist"></div>
+                <div className="song-artist"></div> */}
+                <div className="audio-player">
+                <Player />
             </div>
-            <div className="slider"></div>
-            <div className="right"></div>
-        </div>
+            </div>
+
+        // </div>
     )
 }
