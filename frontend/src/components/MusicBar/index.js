6ceb224aae962272de8b5a5f98d1401602d6982d
luke <lukebraun88@att.net>
2021-04-21 17:54:02 -0500

    song info in player

--- frontend/src/components/MusicBar/index.js
+++ frontend/src/components/MusicBar/index.js
@@ -7,25 +7,27 @@ const Player = () => {
     const songFile = useSelector((state) => state.currentSong.filePath);
 
     return (
-    <AudioPlayer
+        <AudioPlayer
         autoPlay
         src={songFile}
         onPlay={e => console.log("onPlay")}
-    // other props here
+        // other props here
     />
     )
 }
 
 
 export default function MusicBar() {
+    const artist = useSelector((state) => state.currentSong.artist);
+    const title = useSelector((state) => state.currentSong.name);
     return (
         <div className="music-bar">
             {/* <div className="song-info">
                 <div className="album-image"></div>
                 <div className="song-artist"></div> */}
                 <div className="song_info">
-                    <p className="title_musicBar">example title</p>
-                    <p className="artist_musicBar">examlpe artist</p>
+                    <p className="title_musicBar">{title}</p>
+                    <p className="artist_musicBar">{artist}</p>
                 </div>
                 <div className="audio-player">
                 <Player />
